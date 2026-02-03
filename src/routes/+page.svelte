d70178fd4d4bf876f3dc765b311f2025683b0f16
eukaualima <mavericksamp2018@gmail.com>
2026-02-03 17:57:17 -0300

    primeiro commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    let ola_mundo = "Olá, mundo! 🌍"; // Win + .
+</script>
+
+<main>
+    <h1>{ola_mundo}</h1>
+</main>
+
+<style>
+    main
+    {
+        font-family: 'Courier New', Courier, monospace;
+        align-items: center;
+        padding: 2rem;
+    }
+</style>
