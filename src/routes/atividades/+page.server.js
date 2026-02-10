335d23091598e00b780ef04ce021bc1ff89e29ec
eukaualima <mavericksamp2018@gmail.com>
2026-02-10 18:08:20 -0300

    criação da primeira rota

---
+++ src/routes/atividades/+page.server.js
@@ -0,0 +1,57 @@
+import { AtividadeDAO } from "$lib/server/DAO/AtividadeDAO";
+import { fail } from "@sveltejs/kit";
+
+/*** @type {import('./$types').PageServerLoad} */
+export async function load()
+{
+    const dao = new AtividadeDAO();
+
+    try
+    {
+        const lista = await dao.buscarTodos();
+
+        return {
+            lista: JSON.parse(JSON.stringify(lista)), // Serializando os dados
+            erro: null
+        };
+    }
+    catch (erro)
+    {
+        console.log(erro);
+
+        return {
+            lista: [],
+            erro: `Erro ao carregar a lista: ${erro}`
+        }
+    }
+}
+
+/*** @type {import('./$types').Actions} */
+export const actions =
+{
+    deletar: async ({ request }) =>
+    {
+        const data = await request.formData();
+        const id = data.get('id');
+
+        if (!id)
+        {
+            return fail(400, { erro: 'ID inválido para exclusão.' });
+        }
+
+        const dao = new AtividadeDAO();
+
+        try
+        {
+            await dao.apagar(id);
+            
+            return { sucesso: true };
+        }
+        catch (erro)
+        {
+            console.log(erro);
+
+            return fail(500, { erro: `Erro ao excluir: ${erro.message}` });
+        }
+    }
+}
---
+++ src/routes/atividades/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { enhance } from "$app/forms";
+    import { invalidateAll } from "$app/navigation";
+    
+    export let data;
+</script>
+<div>
+        {#each data.lista as item (item.id)} 
+            <p>ID: {item.id}</p>
+            <p>CLASSIFICAÇÃO: {item.classificacao}</p>
+            <p>MÊS: {item.mes}</p>
+            <p>HORAS: {item.horas}</p>
+            <p>OBSERVAÇÃO: {item.observacao}</p>
+        {/each}
+</div>
